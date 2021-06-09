3ddc0241c7e92646c1d746774ab737f93bb23326
fmv1001 <fmubu1998@gmail.com>
2021-06-09 18:31:57 +0200

    Prototipo de hilo principal creado #10

--- AndroidApp/app/src/main/java/com/example/appstream/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/appstream/MainActivity.java
@@ -1,36 +1,45 @@
 package com.example.appstream;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.net.*;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txv_1;
-    private Socket socket;
-    private static Handler handler;
+    private ImageView imgViewCam1;
+    private ImageView imgViewCam2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Iniciando app...", Toast.LENGTH_SHORT).show();
-        txv_1 = (TextView) findViewById(R.id.msg_1);
-        Button connexion = (Button) findViewById(R.id.button_conex);
-        handler = new Handler();
-        connexion.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(this, "Starting app...", Toast.LENGTH_SHORT).show();
+
+        imgViewCam1 = findViewById(R.id.img_view_cam1);
+        imgViewCam2 = findViewById(R.id.img_view_cam2);
+        Button connexButton = findViewById(R.id.button_conex_cam1);
+        String serverip = "192.168.18.11";
+        connexButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Thread connexionThread = new ServerConnexion(txv_1, handler);
+                Thread rcvThread1 = null;
+                Thread rcvThread2 = null;
+                Thread connexionThread = null;
+                try {
+                    connexionThread = new ServerUDPConnexion(serverip,8888);
+                    rcvThread2 = new RcvThread(imgViewCam2,serverip,9999);
+                    rcvThread1 = new RcvThread(imgViewCam1,serverip,9988);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
                 connexionThread.start();
+                rcvThread2.start();
+                rcvThread1.start();
             }
         });
     }
