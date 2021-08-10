e39d5830b2ce2a9aca551299ec7a88aaccbd2c53
fmv1001 <fmubu1998@gmail.com>
2021-08-10 13:30:55 +0200

    Implementada la comunicación entre fragmentos y actividades en la aplicación #14

--- AndroidApp/app/src/main/java/com/example/appstream/ui/SharedViewModel.java
+++ AndroidApp/app/src/main/java/com/example/appstream/ui/SharedViewModel.java
@@ -1,4 +1,22 @@
 package com.example.appstream.ui;
 
-public class SharedViewModel {
+import android.content.ClipData;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import android.content.ClipData.Item;
+
+import com.example.appstream.ServerTCPConnexion;
+
+public class SharedViewModel extends ViewModel {
+    private final MutableLiveData<String[]> selected = new MutableLiveData<String[]>();
+
+    public void select(String[] serverInf) {
+        selected.setValue(serverInf);
+    }
+
+    public LiveData<String[]> getSelected() {
+        return selected;
+    }
 }
