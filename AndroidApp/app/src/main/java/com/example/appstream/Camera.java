cff2883181b81bfb989c56a122d7047e10e9332c
fmv1001 <fmubu1998@gmail.com>
2021-08-10 13:53:52 +0200

    Pequeñas modificaciones en la interfaz de visualizacion de las cámaras

--- AndroidApp/app/src/main/java/com/example/appstream/Camera.java
+++ AndroidApp/app/src/main/java/com/example/appstream/Camera.java
@@ -1,4 +1,46 @@
 package com.example.appstream;
 
+import androidx.annotation.NonNull;
+
 public class Camera {
+
+    private String ip;
+    private int port;
+    private String name;
+
+    public Camera(String name, String ip, int port){
+        this.ip = ip;
+        this.port = port;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name + " - " + port;
+    }
 }
