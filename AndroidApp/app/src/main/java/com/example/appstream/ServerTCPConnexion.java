8acbbbc2102a51eb2a6649fa42ff7e96b5f9c7f3
fmv1001 <fmubu1998@gmail.com>
2021-08-10 13:51:20 +0200

    Añadida funcionalidad en la conexión con el servidor #8

--- AndroidApp/app/src/main/java/com/example/appstream/ServerTCPConnexion.java
+++ AndroidApp/app/src/main/java/com/example/appstream/ServerTCPConnexion.java
@@ -1,18 +1,28 @@
 package com.example.appstream;
 
-import android.graphics.Bitmap;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.ImageView;
+import android.util.Base64;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+
+import com.example.appstream.ui.CheckCamerasViewModel;
+import com.example.appstream.ui.settings.SettingViewModel;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.io.DataInputStream;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class ServerTCPConnexion extends Thread {
 
@@ -25,36 +35,111 @@ public class ServerTCPConnexion extends Thread {
     private DataOutputStream socketSalida;
     private DataInputStream socketEntrada;
     private boolean running = true;
+    private boolean stringAvailable = false;
+    private boolean newCamera = false, deleteCamera = false, stopServer = false,
+            stopCamera = false, startCamera = false;
+    private static ServerTCPConnexion instance;
+    private Context context;
+    private String nameC = "", ipC = "", liveCameras;
+    private int portC = 0;
+    private static ViewModelStoreOwner activity;
+    private CheckCamerasViewModel checkCamerasViewModel;
 
-    public ServerTCPConnexion(String SERVER_IP, int SERVER_PORT) throws UnknownHostException {
+    public ServerTCPConnexion(String SERVER_IP, int SERVER_PORT, Context context, ViewModelStoreOwner activityOwner)
+            throws UnknownHostException { //, Context context
         serverAddress = InetAddress.getByName(SERVER_IP);
         SERVER_PORT_RECV = SERVER_PORT;
         this.SERVER_IP = SERVER_IP;
+        this.context = context;
+        instance = this;
+        activity = activityOwner;
+    }
+
+    public static ServerTCPConnexion getInstance(ViewModelStoreOwner activityOwner) throws UnknownHostException {
+        if (instance == null)
+            return null;
+        if (activity != null)
+            activity = activityOwner;
+        return instance;
+    }
+
+    public static boolean instanceIsNull() {
+        return instance == null;
     }
 
     @Override
     public void run() {
         try {
             idSenderBuffer = "Hola servidor".getBytes();
-
             sendSocket = new Socket(SERVER_IP, SERVER_PORT_RECV);
             socketSalida = new DataOutputStream(sendSocket.getOutputStream());
             socketEntrada = new DataInputStream(sendSocket.getInputStream());
-            socketSalida.write(1);
+            socketSalida.write(idSenderBuffer);
             connexHandler.sendEmptyMessage(1);
-
         } catch (Exception e) {
             System.err.println("Error en el establecimiento de conexion con servidor: ");
             e.printStackTrace();
         }
+
+        System.out.println("Entramos");
+        byte[] bytes = new byte[512];
+        try {
+            socketEntrada.read(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("salimos");
+
+        liveCameras = new String(bytes);
+        checkCamerasViewModel =
+                new ViewModelProvider((ViewModelStoreOwner) activity).get(CheckCamerasViewModel.class);
+        checkCamerasViewModel.select(liveCameras);
+        stringAvailable = true;
+
+        byte[] idSenderBufferAction;
         while(running){
             try {
-                socketEntrada.readInt();
+                if (newCamera){
+                    idSenderBuffer = "1".getBytes();
+                    socketSalida.write(idSenderBuffer);
+                    idSenderBufferAction = (nameC + "-" + ipC + "-" + String.valueOf(portC)).getBytes();
+                    socketSalida.write(idSenderBufferAction);
+                    newCamera = false;
+                }else if(deleteCamera){
+                    idSenderBuffer = "2".getBytes();
+                    socketSalida.write(idSenderBuffer);
+                    idSenderBufferAction = nameC.getBytes();
+                    socketSalida.write(idSenderBufferAction);
+                    deleteCamera = false;
+                }else if(stopCamera){
+                    idSenderBuffer = "3".getBytes();
+                    socketSalida.write(idSenderBuffer);
+                    idSenderBufferAction = nameC.getBytes();
+                    socketSalida.write(idSenderBufferAction);
+                    stopCamera = false;
+                }else if(startCamera){
+                    idSenderBuffer = "4".getBytes();
+                    socketSalida.write(idSenderBuffer);
+                    idSenderBufferAction = nameC.getBytes();
+                    socketSalida.write(idSenderBufferAction);
+                    startCamera = false;
+                }else if(stopServer) {
+                    idSenderBuffer = "0".getBytes();
+                    socketSalida.write(idSenderBuffer);
+                    stopServer = false;
+                    stop_run();
+                }
             } catch (IOException e) {
+                newCamera = false;
+                deleteCamera = false;
+                newCamera = false;
+                stopCamera = false;
+                startCamera = false;
+                e.printStackTrace();
                 stop_run();
             }
         }
-        stop_run();
+        //stop_run();
     }
 
     class ConnexHandler extends Handler {
@@ -69,8 +154,43 @@ public class ServerTCPConnexion extends Thread {
             socketSalida.close();
             socketEntrada.close();
             sendSocket.close();
+            running = false;
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void add_camera(String name, String ip, int port) {
+        nameC = name;
+        ipC = ip;
+        portC = port;
+        newCamera = true;
+    }
+
+    public void stop_camera(String name){
+        nameC = name;
+        stopCamera = true;
+    }
+
+    public void start_camera(String name){
+        nameC = name;
+        startCamera = true;
+    }
+
+    public void del_camera(String name){
+        nameC = name;
+        deleteCamera = true;
+    }
+
+    public void stop_server(){
+        stopServer = true;
+    }
+
+    public String getInitialCameras(){
+        return liveCameras;
+    }
+
+    public boolean isStringAvailable() {
+        return stringAvailable;
     }
 }
